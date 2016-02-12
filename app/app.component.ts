b5691dc434007073d3455560100e9cc8f2a912f0
GroovyPunch <roman@realizeideas.net>
2016-02-12 16:06:52 +0300

    Small ts change

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,22 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: '/app/hero.html'
 })
 
-export class AppComponent {}
+
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero = 'Windstorm';
+    public heroe : Hero = {
+        id: 1,
+        name: 'Windstorm'
+    }
+}
+
+
+interface Hero {
+    id: number;
+    name: string;
+}
+
