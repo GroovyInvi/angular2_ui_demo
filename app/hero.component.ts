75a8d1e573d8c0363e8784672af060274c3dacd8
GroovyPunch <roman@realizeideas.net>
2016-02-13 23:04:19 +0300

    Finished angular2 tutorial.

---
+++ app/hero.component.ts
@@ -0,0 +1,40 @@
+import {Component,OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import {Router} from "angular2/router";
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: '/app/hero.html',
+    providers: [HeroService],
+    directives: [HeroDetailComponent]
+})
+
+
+export class HeroComponent implements OnInit {
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    constructor(
+        private _router: Router,
+        private _heroService: HeroService) { }
+
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero) { this.selectedHero = hero; }
+
+    gotoDetail() {
+        this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    }
+}
+
+
+
+
