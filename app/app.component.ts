d9d51bd97e913cc123d4a0ff1566d563b0ba9754
GroovyPunch <roman@realizeideas.net>
2016-02-14 17:10:33 +0300

    Added material design, started working on app basic design.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,31 @@
 import { Component }       from 'angular2/core';
-import { HeroService }     from './hero.service';
-import { HeroComponent } from './hero.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
-import {DashboardComponent} from "./dashboard.component";
-import {HeroDetailComponent} from "./hero-detail.component";
 
 @Component({
     selector: 'my-app',
     templateUrl: '/app/template/app.html',
-    styleUrls:['/app/basic.css'],
     directives: [ROUTER_DIRECTIVES],
-    providers: [
-        ROUTER_PROVIDERS,
-        HeroService
-    ]
+    providers: [ROUTER_PROVIDERS]
 })
-
-@RouteConfig([
-    {
-        path: '/dashboard',
-        name: 'Dashboard',
-        component: DashboardComponent,
-        useAsDefault: true
-    },
-    {
-        path: '/detail/:id',
-        name: 'HeroDetail',
-        component: HeroDetailComponent
-    },
-    {
-        path: '/heroes',
-        name: 'Heroes',
-        component: HeroComponent
-    }
-])
+//
+//@RouteConfig([
+//    {
+//        path: '/dashboard',
+//        name: 'Dashboard',
+//        component: DashboardComponent,
+//        useAsDefault: true
+//    },
+//    {
+//        path: '/detail/:id',
+//        name: 'HeroDetail',
+//        component: HeroDetailComponent
+//    },
+//    {
+//        path: '/heroes',
+//        name: 'Heroes',
+//        component: HeroComponent
+//    }
+//])
 export class AppComponent {
     title = 'Tour of Heroes';
 }
---
+++ assets/javascript/basic/general.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Romanovski on 14.02.2016.
+ */
+$('.side-nav.fixed').toggleClass('closed');
